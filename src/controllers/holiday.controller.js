59317b82aeb41ae75482547442bb5a335b10ea60
rodriguesss <viniciusrdefreitas@gmail.com>
2022-01-20 12:04:27 -0300

    remove foreach and adding filter in controller

--- src/controllers/holiday.controller.js
+++ src/controllers/holiday.controller.js
@@ -1,7 +1,3 @@
 export default function FindHoliday(holidays, id) {
-    const month = []
-
-    holidays.forEach(holiday => holiday.date.split("/")[0] === id && month.push(holiday))
-
-    return month
+    return holidays.filter(holiday => holiday.date.split("/")[0] === id)
 }
